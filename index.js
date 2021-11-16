e3032d646a5fbd1ed62abbbf00521a41265d480b
mizan <mizanur919@gmail.com>
2021-11-16 21:43:24 +0600

    initialization

---
+++ index.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+var admin = require("firebase-admin");
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient, MongoRuntimeError } = require('mongodb');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// JWT Token
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch
+        {
+
+        }
+    }
+    next();
+}
+
+// Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4b6iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+        const userCollection = database.collection('users');
+
+        // USER POST
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        })
+
+        // User PUT
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateUser = { $set: user };
+            const result = await userCollection.updateOne(filter, updateUser, options);
+            res.json(result);
+        })
+
+        // Make User ADMIN PUT
+        app.put('/users/admin', verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await userCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'You do not have access to make admin' });
+            }
+
+        });
+
+        // Get Data From User Who Is Admin Or Not
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        // Appointments GET
+        app.get('/appointments', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const date = req.query.date;
+            const query = { email: email, date: date };
+            const cursor = appointmentsCollection.find(query);
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+        })
+
+        // Appointments POST
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+        });
+
+    }
+    finally {
+        //await client.close();
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send("I am from express server");
+});
+
+app.listen(port, () => {
+    console.log('Listening port ', port);
+})
